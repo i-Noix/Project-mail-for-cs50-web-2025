d82989d0465a8eb8674ff276537c31f3dffe7380
Roman Nazarov <rnazarov05@gmail.com>
2025-02-17 16:05:43 +0200

    Implement send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,6 +20,37 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-form').onsubmit = () => {
+    // Get values from the form
+    let recipients = document.querySelector('#compose-recipients').value;
+    let subject = document.querySelector('#compose-subject').value;
+    let body = document.querySelector('#compose-body').value;
+  
+    // Sending a post request
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+  
+      // load folder Sent
+      load_mailbox('sent');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  
+    // Stop form from submit
+    return false;
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +61,8 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  
 }
+
+
