660238481559fb69c2f8826f2e7bbefdc68ee7b6
Roman Nazarov <rnazarov05@gmail.com>
2025-02-19 13:37:03 +0200

    Implement css and bnt reply

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -61,11 +61,11 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email').style.display = 'none';
 
-  // Update table-row
-  document.querySelector('#table-row').innerHTML = '';
-
   // Show the mailbox name
   document.querySelector('#name').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Update table-row
+  document.querySelector('#table-row').innerHTML = '';
   
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
@@ -89,9 +89,11 @@ function load_mailbox(mailbox) {
 
       if (mailbox === 'inbox' && !email.archived) {
         button.innerText = 'Archive';
+        button.setAttribute('class', 'btn btn-warning');
         button.onclick = () => archive_email(email.id, true);
       } else if (mailbox === 'archive' && email.archived) {
         button.innerText = 'Unarchive';
+        button.setAttribute('class', 'btn btn-danger');
         button.onclick = () => archive_email(email.id, false);
       }
 
@@ -106,14 +108,21 @@ function load_mailbox(mailbox) {
         tableRow.style.backgroundColor = '#f2f6fc';
       }
       // Adds an event handler
-      tableRow.onclick = () => load_email(email.id);
+      tableRow.onclick = () => {
+        // Check if emails is unread mark the email as read
+        if (email.read && mailbox === 'inbox') {
+          change_readValue(email.id, mailbox);
+        } else {
+          load_email(email.id, mailbox);
+        }
+      }
       // Add div to page
       document.querySelector('#table-row').append(tableRow);
     });
   })
 }
 
-function load_email(email_id) {
+function load_email(email_id, mailbox) {
 
   // Show the email and and hide other views
   document.querySelector('#emails-view').style.display = 'none';
@@ -126,33 +135,40 @@ function load_email(email_id) {
   .then(emails => {
   // Print email
   console.log(emails);
-  // Check if emails is unread mark the email as read
-  if (emails.read) {
-    change_readValue(emails.id);
-  }
   // Create a new elements
   const maileDetails = document.createElement('div');
   const mailBody = document.createElement('div');
+  const btnReply = document.createElement('button');
   maileDetails.innerHTML = `
     <strong>From:</strong> ${emails.sender}<br>
     <strong>To:</strong> ${emails.recipients}<br>
     <strong>Subject:</strong> ${emails.subject}<br>
     <strong>Timestamp:</strong> ${emails.timestamp}<br>
     `
-  mailBody.innerHTML = emails.body
+  // Create the button Reply and add event handler
+  if (mailbox !== 'sent') {
+    btnReply.innerText = 'Reply';
+    btnReply.setAttribute('class', 'btn btn-primary');
+    btnReply.onclick = () => reply_email(emails.sender, emails.subject, emails.timestamp, emails.body);
+  
+    // Append btn
+    maileDetails.append(btnReply);
+  }
+  mailBody.innerText = emails.body
   // Add elements to the page
   document.querySelector('#email').append(maileDetails, mailBody);
 })
 }
 
-function change_readValue(email_id) {
+function change_readValue(email_id, mailbox) {
   fetch(`/emails/${email_id}`, {
     method: 'PUT',
     body: JSON.stringify({
       read: false
     })
-  }
-)}
+  })
+  .then(() => load_email(email_id, mailbox));
+}
 
 function archive_email(email_id, archived) {
   fetch(`/emails/${email_id}`, {
@@ -164,5 +180,60 @@ function archive_email(email_id, archived) {
   .then(() => load_mailbox('inbox'));
 }
 
+function reply_email(recipient, subject, timestamp, body) {
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  // Pre-fill the composition form
+  document.querySelector('#compose-recipients').value = recipient;
+  // If the subject line already begins with Re delete it
+  let subjectWithoutRe = subject.replace('Re: ', '');
+  document.querySelector('#compose-subject').value = `Re: ${subjectWithoutRe}`;
+  let replyMessage = `On ${timestamp} ${recipient} wrote:\n${body}\n\n`;
+  document.querySelector('#compose-body').value = replyMessage;
+  // Add Event handler for form
+  document.querySelector('#compose-form').onsubmit = () => {
+    // Get values from the form
+    let newRecipients = document.querySelector('#compose-recipients').value;
+    let newSubject = document.querySelector('#compose-subject').value;
+    let newBody = document.querySelector('#compose-body').value;
+    let responseBody = newBody.replace(replyMessage, '');
+
+    // Insert the new body message above the previous messages
+    newBody = `${responseBody}\n\nOn ${timestamp}, ${recipient} wrote:\n${body}`
+    
+  
+    // Sending a post request
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: newRecipients,
+        subject: newSubject,
+        body: newBody
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+  
+      // load folder Sent
+      load_mailbox('sent');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  
+    // Stop form from submit
+    return false;
+  }
+}
 
 
